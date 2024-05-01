9ec64583b30431f7667861aea63ce37db1d507fa
Jasmin Huttunen <jasmin.huttunen@protonmail.com>
2024-05-01 20:46:45 +0300

    update sign in

--- components/AuthScreen.js
+++ components/AuthScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
-const AuthScreen = () => {
+const AuthScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
@@ -19,10 +19,11 @@ const AuthScreen = () => {
             });
     };
 
-    const handleSignIn = () => {
+    const handleSignIn = (navigation) => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 setMessage("User signed in successfully!");
+                navigation.navigate('Home', { screen: 'BookList' });
             })
             .catch((error) => {
                 setMessage(`Error: ${error.message}`);
@@ -39,7 +40,7 @@ const AuthScreen = () => {
             <TextInput placeholder="Email" value={email} onChangeText={setEmail} autoCapitalize="none" />
             <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
             <Button title="Sign Up" onPress={handleSignUp} />
-            <Button title="Sign In" onPress={handleSignIn} />
+            <Button title="Sign In" onPress={() => handleSignIn(navigation)} />
             <Text>{message}</Text>
         </View>
     );
